03318563d17114a9fb23a29262a4e5c5e8d5aaf1
kaykewf13 <caique.wesley13@hotmail.com>
2026-04-15 03:13:48 -0300

    Atualizar o SuperflixProvider.php

--- src/Providers/SuperflixProvider.php
+++ src/Providers/SuperflixProvider.php
@@ -13,7 +13,7 @@ class SuperflixProvider implements MediaProviderInterface, MediaProviderProperti
 {
     use SearchEngine;
 
-    public const BASE_URL = 'https://superflixapi.top/serie/';
+    public const BASE_URL = 'https://animeshentai.net/tag/animes-hentai/';
 
     public const SUCCESS_SIZE_RESPONSE = 262715;
 
@@ -37,7 +37,7 @@ class SuperflixProvider implements MediaProviderInterface, MediaProviderProperti
 
     public function name(): string
     {
-        return 'Superflix';
+        return 'animes hentai';
     }
 
     public function baseUrl(): string
@@ -82,6 +82,6 @@ class SuperflixProvider implements MediaProviderInterface, MediaProviderProperti
 
     public function slug(): string
     {
-        return 'superflix';
+        return 'animeshentai';
     }
 }
